3313a8a4c1f18859f3a71988e178811cfb2e26ba
ChineseYouth1949 <17790520686@163.com>
2025-08-22 18:04:26 +0800

    1.0

--- LearnDirectX12Samples/sample/1-HelloTriangle/shaders.hlsl
+++ LearnDirectX12Samples/sample/1-HelloTriangle/shaders.hlsl
@@ -1,13 +1,8 @@
-//*********************************************************
-//
-// Copyright (c) Microsoft. All rights reserved.
-// This code is licensed under the MIT License (MIT).
-// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
-// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
-// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
-//
-//*********************************************************
+struct VSInput
+{
+    float4 position : POSITION;
+    float4 color : COLOR;
+};
 
 struct PSInput
 {
@@ -15,12 +10,12 @@ struct PSInput
     float4 color : COLOR;
 };
 
-PSInput VSMain(float4 position : POSITION, float4 color : COLOR)
+PSInput VSMain(VSInput input)
 {
     PSInput result;
 
-    result.position = position;
-    result.color = color;
+    result.position = input.position;
+    result.color = input.color;
 
     return result;
 }
