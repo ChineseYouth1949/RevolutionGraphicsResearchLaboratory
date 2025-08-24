99efcd6a8f527caa92fdf3e461ae7fbff43584ed
ChineseYouth1949 <17790520686@163.com>
2025-08-24 23:23:34 +0800

    helloConstBuffer继承helloTriangle

--- LearnDirectX12Samples/sample/2-HelloConstBuffers/HelloConstBuffers.cpp
+++ LearnDirectX12Samples/sample/2-HelloConstBuffers/HelloConstBuffers.cpp
@@ -1,1 +1,3 @@
 #include "HelloConstBuffers.h"
+
+HelloConstBuffers::HelloConstBuffers(UINT width, UINT height, std::wstring name) : HelloTriangle(width, height, name) {}
--- LearnDirectX12Samples/sample/2-HelloConstBuffers/HelloConstBuffers.h
+++ LearnDirectX12Samples/sample/2-HelloConstBuffers/HelloConstBuffers.h
@@ -1,30 +1,24 @@
 #pragma once
 
 #include "DXSample.h"
+#include "HelloTriangle.h"
 
 using namespace DirectX;
 
-class HelloConstBuffers : public DXSample {
+class HelloConstBuffers : public HelloTriangle {
  public:
   HelloConstBuffers(UINT width, UINT height, std::wstring name);
 
-  virtual void OnInit();
-  virtual void OnUpdate();
-  virtual void OnRender();
-  virtual void OnDestroy();
+  // virtual void OnInit();
+  // virtual void OnUpdate();
+  // virtual void OnRender();
+  // virtual void OnDestroy();
 
- private:
-  static const UINT FrameCount = 2;
+  //  protected:
+  //   struct SceneConstantBuffer {
+  //     XMFLOAT4 offset;
+  //     float padding[60];
+  //   };
 
-  struct Vertex {
-    XMFLOAT3 position;
-    XMFLOAT4 color;
-  };
-
-  struct SceneConstantBuffer {
-    XMFLOAT4 offset;
-    float padding[60];
-  };
-
-  static_assert((sizeof(SceneConstantBuffer) % 256) == 0, "Constant Buffer size must be 256-byte aligned");
+  //   static_assert((sizeof(SceneConstantBuffer) % 256) == 0, "Constant Buffer size must be 256-byte aligned");
 };
