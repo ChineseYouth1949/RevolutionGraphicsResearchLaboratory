9d2eff9826f247adefe1f6684d96f7260d91a332
ChineseYouth1949 <17790520686@163.com>
2025-08-24 22:07:53 +0800

    HelloConstBuffers

---
+++ LearnDirectX12Samples/sample/2-HelloConstBuffers/main.cpp
@@ -0,0 +1,6 @@
+#include "HelloTriangle.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
+  HelloTriangle sample(1280, 720, L"DXSample");
+  return Win32Application::Run(&sample, hInstance, nCmdShow);
+}
